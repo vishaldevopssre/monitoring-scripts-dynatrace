3b7e610c30a651ba0a8e656e29844154fb5227a7
vishaldevopssre <130963828+vishaldevopssre@users.noreply.github.com>
2024-04-08 18:40:28 +0530

    Initial Code Commit

---
+++ application-registration.tf
@@ -0,0 +1,12 @@
+{
+    "App-A": {
+        "host-group": "group-a",
+        "delay-in-minutes": 20,
+        "notify": ["app.a.owner@dynatrace.com"]
+    },
+    "App-B": {
+        "host-group": "group-b",
+        "delay-in-minutes": 30,
+        "notify": ["app.b.owner@dynatrace.com"]
+    }
+}
